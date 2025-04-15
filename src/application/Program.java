2a1919ab07c92ec839d6a73e0f1cfef78e8a57a7
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-04-15 15:22:39 -0300

    tratamento de exceções

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		method1();
+
+		System.out.println("End of program");
+
+	}
+
+	private static void method1() {
+		method2();
+	}
+
+	private static void method2() {
+
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			String[] vect = sc.nextLine().split(" "); // split - separador de cada vetor
+			int position = sc.nextInt(); // para capturar a posição do vetor
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
+			System.out.println("Input error!");
+		}
+
+		sc.close();
+
+	}
+
+}
